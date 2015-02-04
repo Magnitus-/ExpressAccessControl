b8fd23b04c56fdc3cfcaef3cf3a6d3b6c1d50612
Eric Vallee <eric_vallee2003@yahoo.ca>
2015-02-04 16:56:46 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,163 @@
+Express-Access-Control
+======================
+
+Module to implement access control based on a user's membership to groups.
+
+It is intended as a dependency for express-user and express-user-local, but could be used on it's own by making use of the customization facilities.
+
+Requirements
+============
+
+- A recent version of Node.js (version 0.10.25 is installed on my machine, later versions should work also, let me know if that is not the case)
+
+- The AuthenticateRoute function is meant to be used with an Express router, although it could feasibly work with frameworks that support a similar API.
+
+Installation
+============
+
+npm install express-access-control
+
+Running Tests
+=============
+
+In the directory where the module is located, run the following 2 commands on the prompt:
+
+- npm install
+- npm test
+
+Usage
+=====
+
+The library has 2 main methods:
+
+Authenticate(Req, Res, Groups, And)
+-----------------------------------
+
+This function, returns true if the request's user matches the expected authentication credentials, else false.
+
+&lt;Req&gt; and &lt;Res&gt; and the usual arguments passed to every Express route handler.
+
+&lt;Groups&gt; is an array of strings correspond to groups that you want to verify that the request's user belongs to. It can be set to null, in which case the call will just verify that the request's user is logged in.
+
+&lt;And&gt; can be set to true or false. If set to true, the function will only match the user if he belongs to all the groups specified in &lt;Groups&gt;.
+
+Ex:
+
+```javascript
+var AccessControl = require('express-access-control');
+
+//Some code
+
+App.put('/Homeworks/Math202/Week2', function(Req, Res, Next) {
+    if(Authenticate(Req, Res, ['Teacher', 'Math'], true))
+    {
+        Next();
+    }
+    {
+        Res.status(401).end();
+    }
+});
+
+//More code
+```
+
+AuthenticateRoute(Options)
+--------------------------
+
+This call is a shortcut to generate a route that authentifies the user (like in the above example).
+
+If the user doesn't pass authentication, the 401 code is returned as a response, else Next() is called to go to the next route handler.
+
+&lt;Options&gt; can take 2 formats:
+
+- An array of groups the user should belong to
+
+In this case, 'Authenticate' is used to authenticate the user with &lt;Groups&gt; set to &lt;Options&gt; and &lt;And&gt; set to false.
+
+- An object of options:
+
+-The 'Group' property specifies the group to check that the user belongs to. If null, the route route only verifies that the user is logged in. Defaults to null.
+-The 'And' proporty specifies whether the user should belong to all the groups. Defaults to false.
+-The 'Not' property specifies whether the user should not belong to the groups instead. Defaults to false.
+
+Ex:
+
+```javascript
+var AccessControl = require('express-access-control');
+
+//Some code
+
+App.posts('/Forums/French', AccessControl.AuthenticateRoute({'Groups': ['Banned'], 'Not': true}));
+
+//More code
+```
+
+Customization
+=============
+
+The library is, by default, dependent on req.session (set by the express-session library) and req.session.User.Memberships (set by the user-store and express-user libraries) being present for each request.
+
+You can alter these expectations with the following calls:
+
+SetGetMemberships(NewGetter)
+----------------------------
+
+Changes the internal function that fetches a user's memberships.
+
+&lt;NewGetter&gt; needs to have the following signature: function(Req, Res)
+
+The arguments are those that Express passes to route handlers. The return value should be an array containing the user's memberships as strings.
+
+AccessControl.SetLoggedIn
+
+SetLoggedIn(NewChecker)
+-----------------------
+
+Changes the internal function that determines if a user is logged in.
+
+&lt;NewChecker&gt; needs to have the following signature: function(Req, Res)
+
+The arguments are those that Express passes to route handlers. The return value should be true if the user is logged in, else false.
+
+Example
+-------
+
+ex:
+
+```javascript
+//Assume our User info is stored in Res.locals.User.Groups instead
+
+var AccessControl = require('express-access-control');
+
+AccessControl.SetGetMemberships(function(Req, Res) {
+    return(Res.locals.User.Groups);
+});
+
+AccessControl.SetLoggedIn(function(Req, Res) {
+    var ToReturn = Res.locals.User ? true : false;
+    return(ToReturn);
+});
+```
+
+Future
+======
+
+This library will probably be augmented with custom response handling and custom memberships testing (via a function argument).
+
+Other potential modifications, would be to change the customization API to allow customization fonctions to be asynchronous (if they need to make a trip to the database for example).
+
+These change will be made as needs arises.
+
+Versions History
+================
+
+1.0.0
+-----
+
+Initial release.
+
+
+
+
+
+
---
+++ test/Tests.js
@@ -0,0 +1,153 @@
+//Copyright (c) 2015 Eric Vallee <eric_vallee2003@yahoo.ca>
+//MIT License: https://raw.githubusercontent.com/Magnitus-/ExpressAccessControl/master/License.txt
+
+var Express = require('express');
+var Http = require('http');
+var AccessControl = require('../lib/ExpressAccessControl');
+
+var Context = {};
+
+function Setup(Routes, Callback)
+{
+    var App = Context['App'] = Express();
+    Routes.forEach(function(Item, Index, List) {
+        App[Item['Method']](Item['Path'], Item['Call']);
+    });
+    Context['Server'] = Http.createServer(Context['App']);
+    Context['Server'].listen(8080, function() {
+        Callback();
+    });
+}
+
+function TearDown(Callback)
+{
+    Context['Server'].close(function() {
+        Callback();
+    });
+}
+
+function Request(Method, Path, Callback) {
+    var Self = this;
+    var RequestObject = {'hostname': 'localhost', 'port': 8080, 'method': Method, 'path': Path};
+    var Req = Http.request(RequestObject, function(Res) {
+        Res.on('end', function() {
+            Callback(Res.statusCode);
+        });
+        Res.setEncoding('utf8');
+        Res.resume();
+    });
+    Req.end();
+};
+
+function GetRoutes(SetUser, SetMembership)
+{
+    var Routes = [];
+    Routes.push({'Method': 'use',
+                'Path': '/:LoggedIn/:Groups',
+                'Call': function(Req, Res, Next) {
+                    if(Req.params.LoggedIn=='Yes')
+                    {
+                        SetUser(Req, Res);
+                        var Groups = Req.params.Groups;
+                        if(Groups!='None')
+                        {
+                            Groups = Groups.split(',');
+                            SetMembership(Req, Res, Groups);
+                        }
+                    }
+                    Next();
+                }});
+    Routes.push({'Method': 'get',
+                 'Path': '/:LoggedIn/:Groups',
+                 'Call': AccessControl.AuthenticateRoute()});
+    Routes.push({'Method': 'get',
+                 'Path': '/:LoggedIn/:Groups/Admin',
+                 'Call': AccessControl.AuthenticateRoute(['Admin'])});
+    Routes.push({'Method': 'get',
+                 'Path': '/:LoggedIn/:Groups/President,Admin',
+                 'Call': AccessControl.AuthenticateRoute(['President', 'Admin'])});
+    Routes.push({'Method': 'get',
+                 'Path': '/:LoggedIn/:Groups/President,Admin/And',
+                 'Call': AccessControl.AuthenticateRoute({'Groups': ['President', 'Admin'], 'And': true})});
+    Routes.push({'Method': 'get',
+                 'Path': '/:LoggedIn/:Groups/Banned/Not',
+                 'Call': AccessControl.AuthenticateRoute({'Groups': ['Banned'], 'Not': true})});
+    Routes.push({'Method': 'use', 
+                 'Path': '/',
+                 'Call': function(Req, Res) {
+                     Res.status(204).end();
+                 }});
+    return(Routes);
+}
+
+function Main(Test) 
+{
+    Test.expect(10);
+    Request('get', '/No/None', function(Status) {
+        Test.ok(Status==401, 'Confirming loggin check works when not logged in.');
+        Request('get', '/No/None/Admin', function(Status) {
+            Test.ok(Status==401, 'Confirming loggin check works with args when not logged in.');
+            Request('get', '/Yes/None', function(Status) {
+                Test.ok(Status==204, 'Confirming loggin check works when logged in.');
+                Request('get', '/Yes/None/Admin', function(Status) {
+                    Test.ok(Status==401, 'Confirming that basic membership check works when user is not a member.');
+                    Request('get', '/Yes/Admin/Admin', function(Status) {
+                        Test.ok(Status==204, 'Confirming that basic membership check works when user is a member.');
+                        Request('get', '/Yes/Admin/President,Admin', function(Status) {
+                            Test.ok(Status==204, 'Confirming that basic multi-group check works.');
+                            Request('get', '/Yes/Admin/President,Admin/And', function(Status) {
+                                Test.ok(Status==401, 'Confirming that negative check with And works.');
+                                Request('get', '/Yes/Admin,President/President,Admin/And', function(Status) {
+                                    Test.ok(Status==204, 'Confirming that positive check with And works.');
+                                    Request('get', '/Yes/None/Banned/Not', function(Status) {
+                                        Test.ok(Status==204, 'Confirming that positive check with Not works.');
+                                        Request('get', '/Yes/Banned/Banned/Not', function(Status) {
+                                            Test.ok(Status==401, 'Confirming that negative check with Not works.');
+                                            Test.done();
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+    
+
+exports.RouteDefault = {
+    'setUp': function(Callback) {
+        var Routes = GetRoutes(function(Req, Res) {Req.session  = {'User': {}};}, function(Req, Res, Groups) {Req.session.User.Memberships = Groups;});
+        Setup(Routes, Callback);
+    },
+    'tearDown': function(Callback) {
+        TearDown(Callback);
+    },
+    'Main': Main
+};
+
+exports.RouteCustomization = {
+    'setUp': function(Callback) {
+        AccessControl.SetLoggedIn(function(Req, Res) {
+            var ToReturn = Res.locals.User ? true : false;
+            return ToReturn;
+        });
+        AccessControl.SetGetMemberships(function(Req, Res) {
+            var ToReturn = Res.locals.User && Res.locals.User.Memberships ? Res.locals.User.Memberships : [];
+            return ToReturn;
+        });
+        var Routes = GetRoutes(function(Req, Res) {
+                                   Res.locals.User = {};
+                               }, 
+                               function(Req, Res, Groups) {
+                                   Res.locals.User.Memberships = Groups;
+                               });
+        Setup(Routes, Callback);
+    },
+    'tearDown': function(Callback) {
+        TearDown(Callback);
+    },
+    'Main': Main
+};
